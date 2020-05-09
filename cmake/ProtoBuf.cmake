266ee2cea1d8c84fd7d4c34c24124943ef395946
Troy Liu <troyliu0105@outlook.com>
2020-05-09 14:09:01 +0800

    A: only use extra conan package

--- cmake/ProtoBuf.cmake
+++ cmake/ProtoBuf.cmake
@@ -1,24 +1,21 @@
 # Finds Google Protocol Buffers library and compilers and extends
 # the standard cmake script with version and python generation support
 
-if (NOT DEFINED CONAN_PROTOBUF_ROOT)
-    if (MSVC)
-        # search using protobuf-config.cmake
-        find_package(Protobuf REQUIRED NO_MODULE)
-        set(PROTOBUF_INCLUDE_DIR ${PROTOBUF_INCLUDE_DIRS})
-    else ()
-        find_package(Protobuf REQUIRED)
-    endif ()
-    list(APPEND Caffe_INCLUDE_DIRS PUBLIC ${PROTOBUF_INCLUDE_DIR})
-    list(APPEND Caffe_LINKER_LIBS PUBLIC ${PROTOBUF_LIBRARIES})
+if (MSVC)
+    # search using protobuf-config.cmake
+    find_package(Protobuf REQUIRED NO_MODULE)
+    set(PROTOBUF_INCLUDE_DIR ${PROTOBUF_INCLUDE_DIRS})
+else ()
+    find_package(Protobuf REQUIRED)
 endif ()
+list(APPEND Caffe_INCLUDE_DIRS PUBLIC ${PROTOBUF_INCLUDE_DIR})
+list(APPEND Caffe_LINKER_LIBS PUBLIC ${PROTOBUF_LIBRARIES})
+
 
 # As of Ubuntu 14.04 protoc is no longer a part of libprotobuf-dev package
 # and should be installed separately as in: sudo apt-get install protobuf-compiler
 if (EXISTS ${PROTOBUF_PROTOC_EXECUTABLE})
     message(STATUS "Found PROTOBUF Compiler: ${PROTOBUF_PROTOC_EXECUTABLE}")
-elseif (DEFINED CONAN_BIN_DIRS_PROTOBUF)
-    set(PROTOBUF_PROTOC_EXECUTABLE ${CONAN_BIN_DIRS_PROTOBUF}/protoc)
 else ()
     message(FATAL_ERROR "Could not find PROTOBUF Compiler")
 endif ()
