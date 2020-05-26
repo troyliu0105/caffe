ec19a56e8114165354b15681c37272c86bbe6428
Troy Liu <troyliu0105@outlook.com>
2020-05-26 18:24:44 +0800

    A: add Turing cuda

--- cmake/Cuda.cmake
+++ cmake/Cuda.cmake
@@ -4,7 +4,7 @@ endif ()
 
 # Known NVIDIA GPU achitectures Caffe can be compiled for.
 # This list will be used for CUDA_ARCH_NAME = All option
-set(Caffe_known_gpu_archs "20 21(20) 30 35 50 60 61 70")
+set(Caffe_known_gpu_archs "20 21(20) 30 35 50 60 61 70 75")
 
 ################################################################################################
 # A function for automatic detection of GPUs installed  (if autodetection is enabled)
@@ -95,6 +95,8 @@ function(caffe_select_nvcc_arch_flags out_variable)
         set(__cuda_arch_bin "50")
     elseif (${CUDA_ARCH_NAME} STREQUAL "Pascal")
         set(__cuda_arch_bin "60 61")
+    elseif (${CUDA_ARCH_NAME} STREQUAL "Turing")
+        set(__cuda_arch_bin "70 75")
     elseif (${CUDA_ARCH_NAME} STREQUAL "All")
         set(__cuda_arch_bin ${Caffe_known_gpu_archs})
     elseif (${CUDA_ARCH_NAME} STREQUAL "Auto")
